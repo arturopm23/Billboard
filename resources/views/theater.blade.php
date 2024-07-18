147e550f88bf0d94d702fc704f76da34dc344b35
arturopm23 <arturopermeg23@gmail.com>
2024-07-18 12:17:45 +0200

    Primera versión de las diferentes vistas, modelos y controladores del proyecto

---
+++ resources/views/theater.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:heading>
+        {{$theater['name']}}
+    </x-slot:heading>
+    <h1>Hello from the theater page :)</h1>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,33 @@
 <?php
 
+use App\Models\Movie;
+use App\Models\Theater;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/film', function () {
-    return 'Films lol';
+Route::get('/movie', function () {
+    return view('movies', [ 'movies' => Movie::all()]);
+});
+
+Route::get('/movie/{id}', function ($id) {
+    $movie = Movie::find($id);
+    return view('movie', ['movie' => $movie]);
+});
+
+
+Route::get('/theater', function () {
+    return view('theaters', [ 'theaters' => Theater::all()]);
+});
+
+Route::get('/theater/{id}', function ($id) {
+    $theater = Theater::find($id);
+    return view('theater', ['theater' => $theater]);
+});
+
+
+Route::get('/contact', function () {
+    return view('contact');
 });
