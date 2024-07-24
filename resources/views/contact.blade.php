acdae4f8a34a937eebaa186fab7ce34c30b85d65
arturopm23 <arturopermeg23@gmail.com>
2024-07-24 12:54:32 +0200

    Model, View and Controller updated to add theaters + updated views and validations

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,4 +3,14 @@
         Contact
     </x-slot:heading>
     <h1>Hello from the Contact page :)</h1>
+    <div style="width: 100%">
+        <iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Carrer%20de%20Roc%20Boronat,%20117,%20127,%20Sant%20Mart%C3%AD,%2008018%20Barcelona+(Cibern%C3%A0rium)&amp;t=p&amp;z=16&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+            <a href="https://www.gps.ie/">gps systems</a>
+        </iframe>
+    </div>
+    <body>
+Cinernàrium is a premier cinema chain renowned for its exceptional movie-going experience in Spain. Each year, over 7 million movie enthusiasts flock to one of its 300 screens across 25 locations in the country's major cities. Notably, Cinernàrium boasts 12 cinemas in Madrid and 8 in Barcelona, offering state-of-the-art facilities and unparalleled comfort.
+
+Cinernàrium is part of the prestigious Cineworld Group, a leading global cinema operator with a presence in 10 countries, comprising over 780 cinemas and 9,500 screens worldwide. In 2018, Cineworld acquired Regal Cinemas, solidifying its position as the second-largest cinema chain globally.
+    </body>
 </x-layout>
--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -4,16 +4,28 @@
     </x-slot:heading>
     
     <div class="space-y-4">
-    @foreach ($movies as $movie)
-            <a href="/movie/{{$movie['id']}}" class="hover:underline block px-4 py-6 border border-gray 200 rounded-lg">
-            <strong>{{$movie['title']}}</strong>
-            <div>
-                Directed by {{$movie['director']}}
+        <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+            @foreach ($movies as $movie)
+            <div class="group relative">
+                <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75">
+                    <img src="https://m.media-amazon.com/images/I/81rNSKJlQuL._AC_UF894,1000_QL80_.jpg" alt="Movie poster for {{$movie['title']}}" class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+                </div>
+                <div class="mt-4 flex justify-between">
+                    <div>
+                        <h3 class="text-sm text-gray-700">
+                            <a href="/movie/{{$movie['id']}}">
+                                <span aria-hidden="true" class="absolute inset-0"></span>
+                                <strong>{{$movie['title']}}</strong>
+                            </a>
+                        </h3>
+                        <p class="mt-1 text-sm text-gray-500">Directed by {{$movie['director']}}</p>
+                    </div>
+                </div>
             </div>
-            </a>
-    @endforeach
+            @endforeach
         </div>
         <div>
             {{ $movies->links() }}
         </div>
+    </div>
 </x-layout>
